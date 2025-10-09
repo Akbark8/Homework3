4a3296a883f9b3461d73024f4768cb199c258a4b
Akbark8 <konokovakbar@gmail.com>
2025-10-09 19:44:22 +0600

    hw3

---
+++ books/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class booksConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'books'
+    verbose_name = 'книги'
---
+++ books/views.py
@@ -0,0 +1,40 @@
+from django.views.generic import DetailView, ListView
+from .models import Book
+from .forms import ReviewForm
+from django.shortcuts import redirect
+
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'books/book_detail.html'
+    context_object_name = 'book'
+
+
+class BookListView(ListView):
+    model = Book
+    template_name = 'books/book_list.html'
+    context_object_name = 'books'
+
+
+def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context['form'] = ReviewForm()
+    context['reviews'] = self.object.reviews.all().order_by('-created_at')
+    # Доп: средняя оценка
+    reviews = self.object.reviews.all()
+    if reviews.exists():
+        context['avg_rating'] = round(sum(r.rating for r in reviews) / len(reviews), 1)
+    else:
+        context['avg_rating'] = 'Нет оценок'
+    return context
+
+
+def post(self, request, *args, **kwargs):
+    self.object = self.get_object()
+    form = ReviewForm(request.POST)
+    if form.is_valid():
+        review = form.save(commit=False)
+        review.book = self.object
+        review.save()
+        return redirect('book_detail', pk=self.object.pk)
+    return self.get(request, *args, **kwargs)
